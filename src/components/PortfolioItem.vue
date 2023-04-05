0dd942b2721d1bdfa3d337de43bc96ed7b74ae18
Władysław <89138444+vlzher@users.noreply.github.com>
2023-04-05 10:22:38 +0200

    added new sprite project to portfolio

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -1,35 +1,52 @@
 <template>
-  <div class="project" @mouseleave="e => mouseLeaveProject(e)" @mouseenter="e => mouseEnterProject(e)">
+  <div
+    class="project"
+    @mouseleave="(e) => mouseLeaveProject(e)"
+    @mouseenter="(e) => mouseEnterProject(e)"
+  >
     <div class="project-head">
-      <img :src="imagePath" alt="" class="img-fluid card-img custom-img">
-      <div class="project-overlay" style="top: 0px;">
-        <h4>{{fText}}</h4>
+      <img :src="imagePath" alt="" class="img-fluid card-img custom-img" />
+      <div class="project-overlay" style="top: 0">
+        <h4>{{ fText }}</h4>
       </div>
-      <div class="project-hover" style="top: 100%;">
+      <div class="project-hover" style="top: 100%">
         <div class="project-body text-center">
-          <h3 class="title">{{sText}}</h3>
+          <h3 class="title">{{ sText }}</h3>
 
-          <ul class="filters filters-tag text-center" :style="{marginTop: '10px'}" @click="e => filterClick(e)">
+          <ul
+            class="filters filters-tag text-center"
+            :style="{ marginTop: '10px' }"
+            @click="(e) => filterClick(e)"
+          >
             tech:
-            <li :style="{marginTop: '5px'}" v-for="tag in tags" ><a>{{ tag }}</a></li>
+            <li :style="{ marginTop: '5px' }" v-for="tag in tags">
+              <a>{{ tag }}</a>
+            </li>
           </ul>
 
           <div class="btn-group" role="group">
-            <a :href="codeURL"  class="btn  project-btn"><i class="fas fa-code"></i> Code</a>
-            <a :href="viewURL" :class="isDisabled ? 'disabled' : ''" class="btn  project-btn"><i class="far fa-eye"></i> View</a>
+            <a
+              :href="codeURL"
+              :class="isDisabledGit ? 'disabled': ''"
+              class="btn project-btn"
+              ><i class="fas fa-code"></i> Code</a
+            >
+            <a
+              :href="viewURL"
+              :class="isDisabled ? 'disabled' : ''"
+              class="btn project-btn"
+              ><i class="far fa-eye"></i> View</a
+            >
           </div>
         </div>
       </div>
     </div>
-
-
   </div>
 </template>
 <script setup>
-import {computed, ref} from "vue";
+import { computed, ref } from "vue";
 
 const isActive = ref(false);
-
 
 const props = defineProps({
   photoURL: Number,
@@ -39,73 +56,66 @@ const props = defineProps({
   codeURL: String,
   viewURL: String,
   isDisabled: Boolean,
-})
+  isDisabledGit: {
+    type: Boolean,
+    default: false,
+  },
+});
 const imagePath = computed(() => {
   switch (props.photoURL) {
     case 1:
-      return new URL('../assets/coin-price-checker.png',import.meta.url)
+      return new URL("../assets/coin-price-checker.png", import.meta.url);
     case 2:
-      return new URL('../assets/dictionary.png',
-          import.meta.url)
+      return new URL("../assets/dictionary.png", import.meta.url);
     case 3:
-      return new URL('../assets/clock.png',
-          import.meta.url)
+      return new URL("../assets/sprite.png", import.meta.url);
     case 4:
-      return new URL('../assets/kanban-board.png',
-          import.meta.url)
+      return new URL("../assets/kanban-board.png", import.meta.url);
     case 5:
-      return new URL('../assets/paintAnalogue_475x232.png',
-          import.meta.url)
+      return new URL("../assets/paintAnalogue_475x232.png", import.meta.url);
     case 6:
-      return new URL('../assets/teamder.png',
-          import.meta.url)
+      return new URL("../assets/teamder.png", import.meta.url);
   }
 });
-  const filterClick = (e) => {
-    e.preventDefault();
-    isActive.value = !isActive.value;
-
-  };
-  const mouseEnterProject = (e) => {
-    e.preventDefault();
-    const projectOverlay = e.target.querySelector(".project-overlay");
-    const projectHover = e.target.querySelector(".project-hover");
-    projectOverlay.style.top = "-100%";
-    projectHover.style.top = "0px";
-  };
-  const mouseLeaveProject = (e) => {
-    e.preventDefault();
-    const projectOverlay = e.target.querySelector(".project-overlay");
-    const projectHover = e.target.querySelector(".project-hover");
-    projectOverlay.style.top = "0px";
-    projectHover.style.top = "100%";
-  };
-
-
+const filterClick = (e) => {
+  e.preventDefault();
+  isActive.value = !isActive.value;
+};
+const mouseEnterProject = (e) => {
+  e.preventDefault();
+  const projectOverlay = e.target.querySelector(".project-overlay");
+  const projectHover = e.target.querySelector(".project-hover");
+  projectOverlay.style.top = "-100%";
+  projectHover.style.top = "0px";
+};
+const mouseLeaveProject = (e) => {
+  e.preventDefault();
+  const projectOverlay = e.target.querySelector(".project-overlay");
+  const projectHover = e.target.querySelector(".project-hover");
+  projectOverlay.style.top = "0px";
+  projectHover.style.top = "100%";
+};
 </script>
 
 <style scoped>
-@media screen and (max-width:1250px) {
-.project:nth-child(2) {
-
-  grid-row: 2 / 3 !important;
-  grid-column: 1 !important;
+@media screen and (max-width: 1250px) {
+  .project:nth-child(2) {
+    grid-row: 2 / 3 !important;
+    grid-column: 1 !important;
+  }
+  .project:nth-child(4) {
+    grid-row: 4 / 5 !important;
+    grid-column: 1 !important;
+  }
+  .project:nth-child(6) {
+    grid-row: 6 / 7 !important;
+    grid-column: 1 !important;
+  }
 }
-.project:nth-child(4) {
-
-  grid-row: 4 / 5 !important;
-  grid-column: 1 !important;
-}
-.project:nth-child(6) {
-
-  grid-row: 6 / 7 !important;
-  grid-column: 1 !important;
-}
-}
-.custom-img{
+.custom-img {
   border: none;
   border-radius: 15px;
-  outline: 2px solid #24263B;
+  outline: 2px solid #24263b;
   background-size: cover;
   background-position: center;
   width: 100%;
@@ -166,7 +176,7 @@ ul.filters.filters-tag > li > a:hover {
 }
 
 .project {
-  outline: 2px solid #24263B;
+  outline: 2px solid #24263b;
   border-radius: 20px;
   grid-column: span 1;
   background-color: rgba(255, 255, 255, 0.8);
@@ -174,17 +184,14 @@ ul.filters.filters-tag > li > a:hover {
   text-align: center;
 }
 .project:nth-child(2) {
-
   grid-row: 1 / 3;
   grid-column: 2;
 }
 .project:nth-child(4) {
-
   grid-row: 2 / 4;
   grid-column: 1;
 }
 .project:nth-child(6) {
-
   grid-row: 2 / 4;
   grid-column: 3;
 }
@@ -192,8 +199,8 @@ ul.filters.filters-tag > li > a:hover {
 .project-head {
   position: relative;
   overflow: hidden;
-  width:100%;
-  height:100%;
+  width: 100%;
+  height: 100%;
   border-radius: 10px;
 }
 
@@ -208,16 +215,14 @@ ul.filters.filters-tag > li > a:hover {
   display: flex;
   padding-top: 45%;
   justify-content: center;
-  color: #FFFF;
+  color: #ffff;
   -webkit-transition: ease-in-out 0.2s;
   -moz-transition: ease-in-out 0.2s;
   -ms-transition: ease-in-out 0.2s;
   transition: ease-in-out 0.2s;
-
 }
 
 .project-hover {
-
   position: absolute;
   top: 100%;
   left: 0;
@@ -266,6 +271,6 @@ h3.title {
 
 .project-btn:hover {
   background-color: rgba(254, 70, 28, 1);
-  color: #FFFFFF;
+  color: #ffffff;
 }
 </style>
--- src/components/PortfolioSection.vue
+++ src/components/PortfolioSection.vue
@@ -8,7 +8,7 @@
     <div class="portfolio-container">
       <PortfolioItem :photoURL=1 fText="Crypto Price Checker" sText="Real-Time Crypto Price App" :tags="['Vue', 'LocalStorage', 'WS']" codeURL="https://github.com/vlzher/crypto-price-checker" viewURL="https://crypto-price-graph.netlify.app/"/>
       <PortfolioItem :photoURL=2 fText="Dictionary" sText="Word-Collecting App Email: test@test.com Pass: test " :tags="['React','Redux','SCSS','REST API']" codeURL="https://github.com/vlzher/dictionary-v2.0" viewURL="https://dictionary-shshshsk-vlzher.netlify.app/login"/>
-      <PortfolioItem :photoURL=3 fText="Clock" sText="Clock" :tags="['React','SCSS']" codeURL="https://github.com/vlzher/clock" viewURL="https://custom-clock-vlzher.netlify.app/"/>
+      <PortfolioItem :photoURL=3 fText="Sprite NFT" :is-disabled-git=true :isDisabled=false sText="Sprite NFT APP" :tags="['React','SCSS','Ether.js']" codeURL="https://github.com/vlzher/" viewURL="https://sprite-nft.netlify.app/"/>
       <PortfolioItem :photoURL=4 fText="Kanban Board" sText="Management Tool for visualizing work" :tags="['React','Redux','Drag and Drop API']" codeURL="https://github.com/vlzher/kanban-board" viewURL="https://kanban-desk.netlify.app/"/>
       <PortfolioItem :photoURL=5 fText="Paint Analogue" sText="Simple graphic editor" :tags="['React','Canvas', 'WebSocket']" codeURL="https://github.com/vlzher/PaintAnalogue" viewURL="https://paint-analogue.netlify.app/"/>
       <PortfolioItem :photoURL=6 fText="Teamder" :isDisabled=true sText="Currently In Development" :tags="['Vue','Tailwind CSS','Vuex','JWT','REST API']" codeURL="https://github.com/vlzher/teamder"/>
